7d502a4129000f74813da9d155cc193a70e02656
MargoBil <margo10011990@gmail.com>
2020-06-04 16:24:15 +0300

    task-2

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import user from './../data/user.json';
 import Profile from './Task-1/Profile/Profile';
 import Descriptions from './Task-1/Description/Description';
 import Stats from './Task-1/Stats/Stats';
+import Statistics from './Task-2/Statistics';
+import statisticalData from './../data/statistical-data.json';
+
 
 const App = () => (
   <>
@@ -13,6 +16,7 @@ const App = () => (
     avatar={user.avatar}/>
     <Stats followers = {user.stats.followers} views = {user.stats.views} likes= {user.stats.likes}/>
     </Profile>
+    <Statistics title = "UPLOAD STATS" stats = {statisticalData} />
   </>
 );
 
