094df64a787dfc957f3d1d0519b0039c30483d93
MargoBil <margo10011990@gmail.com>
2020-06-05 12:47:19 +0300

    Updates

---
+++ precache-manifest.e2676204eeaccdbace120a79e158ee1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7881707abfe1e296c388ce99258c601",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "e144d6bb38c3bbdce9de",
+    "url": "/goit-react-hw-01-components/static/css/main.99d975f8.chunk.css"
+  },
+  {
+    "revision": "83fa515422a179dbbce6",
+    "url": "/goit-react-hw-01-components/static/js/2.5b564931.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.5b564931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e144d6bb38c3bbdce9de",
+    "url": "/goit-react-hw-01-components/static/js/main.d177539a.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
